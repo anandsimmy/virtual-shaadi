921b0de073a8a61e1f563f2316edbaac1edcb476
Anand Simmy <anandsimmy7@gmail.com>
2021-03-30 08:34:57 +0530

    added cors

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express= require('express')
 const app= express()
 const server= require('http').Server(app)
 const io= require('socket.io')(server, { 
-    
+    cors: {
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST']
+    }
 })
 
 
