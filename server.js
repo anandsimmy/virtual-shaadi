f0b7461eecb2bf7922b4be7582432d2c7dc74881
Anand Simmy <anandsimmy7@gmail.com>
2021-03-30 08:34:43 +0530

    removed cors

--- server.js
+++ server.js
@@ -2,10 +2,7 @@ const express= require('express')
 const app= express()
 const server= require('http').Server(app)
 const io= require('socket.io')(server, { 
-    cors: {
-        origin: 'http://localhost:3000',
-        methods: ['GET', 'POST']
-    }
+    
 })
 
 
