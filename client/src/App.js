d93900c581acc17ae8451c3e352ec29306ac81dd
Anand Simmy <anandsimmy7@gmail.com>
2021-03-31 08:53:41 +0530

    added packages

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React, { useState, useEffect, useRef } from 'react'
+import Button from '@material-ui/core/Button'
+import IconButton from '@material-ui/core/IconButton'
+import TextField from '@material-ui/core/TextField'
+import AssignmentIcon from '@material-ui/core/Assignment'
+import PhoneIcon from '@material-ui/core/Phone'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
+import Peer from 'simple-peer'
+import io from 'socket.io-client'
 import './App.css';
 
-function App() {
+const socket= io.connect('http://localhst:5000')
+
+const App= () => {
+
+  const myVideo= useRef()
+  const userVideo= useRef()
+  const connectionRef= useRef()
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
     </div>
   );
 }
