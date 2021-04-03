c66b5d9730b67050895c30dfb723dceee31e4169
Anand Simmy <anandsimmy7@gmail.com>
2021-04-03 06:23:32 +0530

    added socket events

--- client/src/App.js
+++ client/src/App.js
@@ -13,9 +13,35 @@ const socket= io.connect('http://localhst:5000')
 
 const App= () => {
 
+  const [me, setMe]= useState('')
+  const [stream, setStream]= useState('')
+  const [caller, setCaller]= useState('')
+  const [receivingCall, setReceivingCall]= useState('')
+  const [name, setName]= useState('')
+  const [callerSignal, setCallerSignal]= useState('')
+
   const myVideo= useRef()
   const userVideo= useRef()
   const connectionRef= useRef()
+
+  useEffect(() => {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then((stream) => {
+      setStream(stream)
+      myVideo.current.srcObject= stream
+    })
+
+    socket.on('me', id => {
+      setMe(id)
+    })
+
+    socket.on('callUser', (data) => {
+        setReceivingCall(true)
+        setCaller(data.from)
+        setName(data.name)
+        setCallerSignal(data.signal)
+    })
+
+  }, [])
 
   return (
     <div className="App">
